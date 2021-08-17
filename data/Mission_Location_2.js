da2f5e6ee028ce9e68320280e624177f7515b34d
eantoh <antoh.gh@gmail.com>
2021-08-17 20:03:03 +0000

    Add files via upload

---
+++ data/Mission_Location_2.js
@@ -0,0 +1,28 @@
+var json_Mission_Location_2 = {
+"type": "FeatureCollection",
+"name": "Mission_Location_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+    { "type": "Feature", "properties": { "Capital": "Cotonou" }, "geometry": { "type": "Point", "coordinates": [ 2.392222, 6.368634 ] } },
+    { "type": "Feature", "properties": { "Capital": "Ouagadougou" }, "geometry": { "type": "Point", "coordinates": [ -1.51966, 12.371428 ] } },
+    { "type": "Feature", "properties": { "Capital": "Yaoundé" }, "geometry": { "type": "Point", "coordinates": [ 11.502075, 3.848033 ] } },
+    //{ "type": "Feature", "properties": { "Capital": "Praia" }, "geometry": { "type": "Point", "coordinates": [ -23.513327, 14.93305 ] } },
+    { "type": "Feature", "properties": { "Capital": "N'Djamena" }, "geometry": { "type": "Point", "coordinates": [ 15.055742, 12.134846 ] } },
+    { "type": "Feature", "properties": { "Capital": "Yamoussoukro" }, "geometry": { "type": "Point", "coordinates": [ -5.289343, 6.827623 ] } },
+    //{ "type": "Feature", "properties": { "Capital": "Malabo" }, "geometry": { "type": "Point", "coordinates": [ 8.737104, 3.750412 ] } },
+    //{ "type": "Feature", "properties": { "Capital": "Libreville" }, "geometry": { "type": "Point", "coordinates": [ 9.467268, 0.416198 ] } },
+    { "type": "Feature", "properties": { "Capital": "Banjul" }, "geometry": { "type": "Point", "coordinates": [ -16.579032, 13.454876 ] } },
+    { "type": "Feature", "properties": { "Capital": "Accra" }, "geometry": { "type": "Point", "coordinates": [ -0.186964, 5.603717 ] } },
+    { "type": "Feature", "properties": { "Capital": "Conakry" }, "geometry": { "type": "Point", "coordinates": [ -13.578401, 9.641185 ] } },
+    //{ "type": "Feature", "properties": { "Capital": "Bissau" }, "geometry": { "type": "Point", "coordinates": [ -15.617794, 11.881655 ] } },
+    { "type": "Feature", "properties": { "Capital": "Monrovia" }, "geometry": { "type": "Point", "coordinates": [ -10.760524, 6.290743 ] } },
+    { "type": "Feature", "properties": { "Capital": "Bamako" }, "geometry": { "type": "Point", "coordinates": [ -8.002889, 12.639232 ] } },
+    { "type": "Feature", "properties": { "Capital": "Nouakchott" }, "geometry": { "type": "Point", "coordinates": [ -15.958237, 18.07353 ] } },
+    { "type": "Feature", "properties": { "Capital": "Niamey" }, "geometry": { "type": "Point", "coordinates": [ 2.125385, 13.511596 ] } },
+    { "type": "Feature", "properties": { "Capital": "Abuja" }, "geometry": { "type": "Point", "coordinates": [ 7.398574, 9.076479 ] } },
+    //{ "type": "Feature", "properties": { "Capital": "São Tomé" }, "geometry": { "type": "Point", "coordinates": [ 6.733343, 0.330192 ] } },
+    { "type": "Feature", "properties": { "Capital": "Dakar" }, "geometry": { "type": "Point", "coordinates": [ -17.366029, 14.764504 ] } },
+    { "type": "Feature", "properties": { "Capital": "Freetown" }, "geometry": { "type": "Point", "coordinates": [ -13.231722, 8.465677 ] } },
+    //{ "type": "Feature", "properties": { "Capital": "Lomé" }, "geometry": { "type": "Point", "coordinates": [ 1.231362, 6.172497 ] } }
+]
+}
